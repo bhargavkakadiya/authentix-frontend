4e64d9a75606482e3fcccb710d5739fd903c3466
Bhargav Kakadiya <kakadiyabhargav7@gmail.com>
2023-08-21 23:38:09 -0700

    warning fix

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -33,9 +33,9 @@ export default function EventCard() {
               viewBox="0 0 20 20"
               fill="currentColor">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             <p className="text-gray-500 font-normal text-sm">John Doe</p>
